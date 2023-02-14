f3573fa9e2e68996d834fd5ac1a559b3537ac69d
Sergi Casquero <sergicasquero@gmail.com>
2023-02-14 19:43:55 +0100

    Add Header and Layout components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): JSX.Element => {
+  return (
+    <HeaderStyled>
+      <h1>Task List App</h1>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
